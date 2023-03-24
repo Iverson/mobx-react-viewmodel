d4d8bf22c209cc314aca5bb2a8cb6bf5cb6099fe
Kirill Baldin <kirill.baldin@alberblanc.com>
2023-03-24 21:33:14 +0400

    add sb and docs for args sintaxis

--- stories/useViewModel.stories.tsx
+++ stories/useViewModel.stories.tsx
@@ -625,3 +625,131 @@ export const WithReactiveProps = () => {
     },
   },
 };
+
+/// ------------------------------------------
+// Example of view-models with Dependency Injection
+//
+type User = {
+  id: string;
+  name: string;
+};
+class UsersStore {
+  private users: Record<string, User> = {
+    'user-1-id': {
+      id: 'user-1-id',
+      name: 'user-1-name',
+    },
+  };
+
+  getUser(id: string): User | undefined {
+    return this.users[id];
+  }
+}
+const userStore = new UsersStore();
+
+interface AdvancedDIViewModelProps {
+  userId: string;
+}
+class AdvancedDIViewModel {
+  @observable.ref
+  props: AdvancedDIViewModelProps;
+
+  private usersStore: UsersStore;
+
+  constructor(props: AdvancedDIViewModelProps, usersStore: UsersStore) {
+    makeObservable(this);
+    this.props = props;
+    this.usersStore = usersStore;
+  }
+
+  @computed
+  get user() {
+    return this.usersStore.getUser(this.props.userId);
+  }
+}
+
+const AdvancedDIExample = observer(function AdvancedDIExample$() {
+  // Pass userStore to view-model via array of dependencies constructor argument
+  const viewModel = useViewModel(
+    AdvancedDIViewModel,
+    { userId: 'user-1-id' },
+    [userStore]
+  );
+
+  return (
+    <div>
+      <p>User from UsersStore: </p>
+      <pre>{viewModel.user ? JSON.stringify(viewModel.user) : '-'}</pre>
+    </div>
+  );
+});
+
+export const WithDependencyInjection = () => {
+  return <AdvancedDIExample />;
+};
+WithDependencyInjection.parameters = {
+  controls: { expanded: true },
+  docs: {
+    source: {
+      code: `
+type User = {
+  id: string;
+  name: string;
+};
+class UsersStore {
+  private users: Record<string, User> = {
+    'user-1-id': {
+      id: 'user-1-id',
+      name: 'user-1-name',
+    },
+  };
+
+  getUser(id: string): User | undefined {
+    return this.users[id];
+  }
+}
+const userStore = new UsersStore();
+
+interface AdvancedDIViewModelProps {
+  userId: string;
+}
+class AdvancedDIViewModel {
+  @observable.ref
+  props: AdvancedDIViewModelProps;
+
+  private usersStore: UsersStore;
+
+  constructor(props: AdvancedDIViewModelProps, usersStore: UsersStore) {
+    makeObservable(this);
+    this.props = props;
+    this.usersStore = usersStore;
+  }
+
+  @computed
+  get user() {
+    return this.usersStore.getUser(this.props.userId);
+  }
+}
+
+const AdvancedDIExample = observer(function AdvancedDIExample$() {
+  // Pass userStore to view-model via factory-hook
+  const viewModel = useViewModel(
+    AdvancedDIViewModel,
+    { userId: 'user-1-id' },
+    [userStore]
+
+  );
+
+  return (
+    <div>
+      <p>User from UsersStore: </p>
+      <pre>{viewModel.user ? JSON.stringify(viewModel.user) : '-'}</pre>
+    </div>
+  );
+});
+      `,
+      language: 'jsx',
+      type: 'auto',
+    },
+  },
+};
