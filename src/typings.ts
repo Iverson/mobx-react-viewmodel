ac6a4b74b922700da6c7216e78518b4ca1bb725b
Alexey Krasman <a.krasman@softpro.com>
2023-03-25 20:16:43 +0400

    Fix lint

--- src/typings.ts
+++ src/typings.ts
@@ -14,7 +14,7 @@ interface BaseViewModel {
 
 // View-model interface with reactive Props from React component
 export type ViewModelProps = {};
-export type ViewModelArgs = Array<unknown>
+export type ViewModelArgs = Array<unknown>;
 export interface ViewModelWithProps<P extends ViewModelProps>
   extends BaseViewModel {
   // You should use @observable.ref for props in your view-model classes
@@ -27,7 +27,7 @@ export type IViewModelClass<
   P extends ViewModelProps | undefined = undefined,
   Args extends ViewModelArgs = ViewModelArgs
 > = P extends {} //
-  ? Args extends []
+  ? Args extends unknown[]
     ? { new (props: P, ...args: Args): T }
     : { new (props: P, ...args: Args): T } // HACK: this place should be { new (props: P): T }
-  : { new (): T }
+  : { new (): T };
